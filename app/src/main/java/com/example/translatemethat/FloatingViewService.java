3fec967efa91292101db384e0262f15fb598da22
Nelson <nelsonrocha2727@gmail.com>
2023-08-29 16:53:02 +0100

    Create sketch of a bubble widget app
    Signed-off-by: Nelson <nelsonrocha2727@gmail.com>

---
+++ app/src/main/java/com/example/translatemethat/FloatingViewService.java
@@ -0,0 +1,124 @@
+package com.example.translatemethat;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+
+import java.util.Calendar;
+
+public class FloatingViewService extends Service implements View.OnClickListener {
+
+
+    private WindowManager mWindowManager;
+    private View mFloatingView;
+    private View collapsedView;
+    private View expandedView;
+
+    public FloatingViewService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+        //getting the widget layout from xml using layout inflater
+        mFloatingView = LayoutInflater.from(this).inflate(R.layout.overlay_layout, null);
+
+        //setting the layout parameters
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+
+        //getting windows services and adding the floating view to it
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mWindowManager.addView(mFloatingView, params);
+
+
+        //getting the collapsed and expanded view from the floating view
+        collapsedView = mFloatingView.findViewById(R.id.layoutCollapsed);
+        expandedView = mFloatingView.findViewById(R.id.layoutExpanded);
+
+        //adding click listener to close button and expanded view
+        mFloatingView.findViewById(R.id.buttonClose).setOnClickListener(this);
+        //mFloatingView.findViewById(R.id.collapsed_iv).setOnClickListener(this);
+        expandedView.setOnClickListener(this);
+
+        //adding an touchlistener to make drag movement of the floating widget
+        mFloatingView.findViewById(R.id.relativeLayoutParent).setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+
+            private static final int MAX_CLICK_DURATION = 125;
+            private long startClickTime;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        startClickTime = Calendar.getInstance().getTimeInMillis();
+                        initialX = params.x;
+                        initialY = params.y;
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+                        return true;
+
+                    case MotionEvent.ACTION_UP:
+                        long clickDuration = Calendar.getInstance().getTimeInMillis() - startClickTime;
+                        if(clickDuration < MAX_CLICK_DURATION) {
+                            collapsedView.setVisibility(View.GONE);
+                            expandedView.setVisibility(View.VISIBLE);
+                        }
+                        return true;
+
+                    case MotionEvent.ACTION_MOVE:
+                        //this code is helping the widget to move around the screen with fingers
+                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+                        mWindowManager.updateViewLayout(mFloatingView, params);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mFloatingView != null) mWindowManager.removeView(mFloatingView);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.layoutExpanded:
+                //switching views
+                collapsedView.setVisibility(View.VISIBLE);
+                expandedView.setVisibility(View.GONE);
+                break;
+
+            case R.id.buttonClose:
+                //closing the widget
+                stopSelf();
+                break;
+        }
+    }
+}
--- app/src/main/java/com/example/translatemethat/MainActivity.kt
+++ app/src/main/java/com/example/translatemethat/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.translatemethat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.widget.TextView
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.translatemethat.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,15 +22,43 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            askPermission()
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            startService(Intent(this@MainActivity, FloatingViewService::class.java))
+            finish()
+        } else if (Settings.canDrawOverlays(this)) {
+            startService(Intent(this@MainActivity, FloatingViewService::class.java))
+            finish()
+        } else {
+            askPermission()
+            Toast.makeText(
+                this,
+                "You need System Alert Window Permission to do this",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        Log.e("MyTag", "Log.e() example")
+
         // Example of a call to a native method
         binding.sampleText.text = stringFromJNI()
+    }
+
+    private fun askPermission() {
+        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+        intent.data = Uri.parse("package:$packageName")
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    external fun stringFromJNI(): String
+    private external fun stringFromJNI(): String
 
     companion object {
         // Used to load the 'native-lib' library on application startup.
